b7afb0a19619a7e212f8ae6de31ea6a2ea057254
Sam <sam20060223@gmail.com>
2023-11-29 21:32:43 +0800

    Initial Commit

---
+++ CountDownDate/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CountDownDate
+//
+//  Created by 江祐鈞 on 2023/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CountDownDate/CountDownDateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountDownDateApp.swift
+//  CountDownDate
+//
+//  Created by 江祐鈞 on 2023/11/29.
+//
+
+import SwiftUI
+
+@main
+struct CountDownDateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
